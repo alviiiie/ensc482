e46c8a42feeb7e1bedf11fe7a3b54b42efc924df
Jordan <32210039+jla522@users.noreply.github.com>
2021-08-14 20:06:00 -0700

    Updated gesture recognition and successive gesture

--- Assets/Test/Script/HandVisualizer.cs
+++ Assets/Test/Script/HandVisualizer.cs
@@ -19,12 +19,39 @@ namespace MediaPipe.HandPose {
         [SerializeField] ParticleSystem Snow = null;
         [SerializeField] ParticleSystem Fire = null;
 
+        // Constructor and initial values
+        fingers getFingers = new fingers();
+        int gesture = 0;
+        int flag = 0;
+
         #endregion
 
         #region Private members
 
         HandPipeline _pipeline;
         (Material keys, Material region) _material;
+
+        // Anchor points
+        private static Vector3 thumb1;
+        private static Vector3 thumb2;
+        private static Vector3 thumb3;
+        private static Vector3 thumb4;
+        private static Vector3 index1;
+        private static Vector3 index2;
+        private static Vector3 index3;
+        private static Vector3 index4;
+        private static Vector3 middle1;
+        private static Vector3 middle2;
+        private static Vector3 middle3;
+        private static Vector3 middle4;
+        private static Vector3 ring1;
+        private static Vector3 ring2;
+        private static Vector3 ring3;
+        private static Vector3 ring4;
+        private static Vector3 pinky1;
+        private static Vector3 pinky2;
+        private static Vector3 pinky3;
+        private static Vector3 pinky4;
 
         #endregion
 
@@ -58,22 +85,73 @@ namespace MediaPipe.HandPose {
 
             // UI update
             _mainUI.texture = _webcam.Texture;
-            Debug.Log("POSITION-> " + getPosition());
+            
 
-            if (getPosition() == 1)
+            // Get the positions of the hand
+            thumb1 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Thumb1);
+            thumb2 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Thumb2);
+            thumb3 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Thumb3);
+            thumb4 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Thumb4);
+            index1 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index1);
+            index2 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index2);
+            index3 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index3);
+            index4 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index4);
+            middle1 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Middle1);
+            middle2 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Middle2);
+            middle3 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Middle3);
+            middle4 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Middle4);
+            ring1 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Ring1);
+            ring2 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Ring2);
+            ring3 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Ring3);
+            ring4 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Ring4);
+            pinky1 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Pinky1);
+            pinky2 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Pinky2);
+            pinky3 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Pinky3);
+            pinky4 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Pinky4);
+
+            gesture = getFingers.getPosition(thumb4,thumb3,thumb2,thumb1,index4,index3,index2,index1,middle4,middle3,middle2,middle1,ring4,ring3,ring2,ring1,pinky4,pinky3,pinky2,pinky1);
+            Debug.Log("POSITION-> " + gesture);
+            /*
+            if (gesture == 1)
                 playLightning(_pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index4));
-            else if (getPosition() == 2)
+            else if (gesture == 2)
                 playFire(_pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index4));
-            else if (getPosition() == 3)
+            else if (gesture == 3)
                 playSnow(_pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index4));
-            else if (getPosition() == 4)
+            else if (gesture == 4)
             {
                 playSnow(_pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index4));
                 playLightning(_pipeline.GetKeyPoint(HandPipeline.KeyPoint.Thumb4));
-            }else if (getPosition() == 5)
+            }else if (gesture == 5)
             {
                 playFire(_pipeline.GetKeyPoint(HandPipeline.KeyPoint.Middle4));
                 playLightning(_pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index4));
+            }
+            */
+            if (gesture == 1){
+                flag = 1;
+            }
+
+            if (flag == 1){
+                if (gesture == 2){
+                    playFire(_pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index4));
+                }
+                else if (gesture == 3){
+                    playLightning(_pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index4));
+                }
+                else if (gesture == 4){ 
+                    playSnow(_pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index4));
+                }
+                else if (gesture == 5){
+
+                }
+                else if (gesture == 0){
+                    // Do nothing
+                }
+                else if (gesture == -1){
+                    //stop();
+                    flag = 0;
+                }
             }
 
 
@@ -94,110 +172,7 @@ namespace MediaPipe.HandPose {
 
 
         #region gesture positions
-        int getPosition()
-        {
-            if (!(thumb()) && !(index()) && !(middle()) && !(ring()) && !(pinky()))
-            {        // fist
-                return 0;       // Should ignore 0
-            }
-            else if (!(thumb()) && !(index()) && !(middle()) && !(ring()) && (pinky()))
-            {       // Trigger
-                return 1;
-            }
-            else if (!(thumb()) && (index()) && (middle()) && !(ring()) && !(pinky()))
-            {         // Gesture 1
-                return 2;
-            }
-            else if ((thumb()) && !(index()) && !(middle()) && !(ring()) && (pinky()))
-            {         // Gesture 2
-                return 3;
-            }
-            else if (!(thumb()) && (index()) && !(middle()) && !(ring()) && (pinky()))
-            {        // Gesture 3
-                return 4;
-            }
-            else if (!(thumb()) && !(index()) && (middle()) && (ring()) && (pinky()))
-            {          // Gesture 4
-                return 5;
-            }
-            else
-            {
-                return -1;
-            }
-        }
 
-        public bool thumb()
-        {
-            Vector3 p4 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Thumb4);
-            Vector3 p3 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Thumb3);
-            if (p4.x < p3.x)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-
-        bool index()
-        {
-            Vector3 p8 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index4);
-            Vector3 p7 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index3);
-            Vector3 p6 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Index2);
-            if ((p8.y < p7.y) && (p7.y < p6.y))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        bool middle()
-        {
-            Vector3 p12 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Middle4);
-            Vector3 p11 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Middle3);
-            Vector3 p10 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Middle2);
-            if ((p12.y < p11.y) && (p11.y < p10.y))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        bool ring()
-        {
-            Vector3 p16 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Ring4);
-            Vector3 p15 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Ring3);
-            Vector3 p14 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Ring2);
-            if ((p16.y < p15.y) && (p15.y < p14.y))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        bool pinky()
-        {
-            Vector3 p20 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Pinky4);
-            Vector3 p19 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Pinky3);
-            Vector3 p18 = _pipeline.GetKeyPoint(HandPipeline.KeyPoint.Pinky2);
-            if ((p20.y < p19.y) && (p19.y < p18.y))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
         #endregion
     
         public void playLightning(Vector3 index)
@@ -217,6 +192,13 @@ namespace MediaPipe.HandPose {
         {
 
             Snow.Play();
+        }
+
+        public void stop()
+        {
+            //Lightning.Stop();
+            //Snow.Stop();
+            //Fire.Stop();
         }
     
     }
